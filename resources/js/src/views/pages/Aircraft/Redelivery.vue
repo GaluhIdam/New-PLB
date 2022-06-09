e45e0dc860d868d2e3c51d6a8bf17970671f7560
Fatkhur Ridlwan <fridlwan@gmail.com>
2022-06-09 20:23:56 +0700

    Aircraft redelivery frontend

--- resources/js/src/views/pages/Aircraft/Redelivery.vue
+++ resources/js/src/views/pages/Aircraft/Redelivery.vue
@@ -1,154 +1,244 @@
 <template>
-  <div>
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Aircraft Redelivery</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
-              <li class="breadcrumb-item"><a href="/mutation">Mutasi Pesawat</a></li>
-              <li class="breadcrumb-item active">Aircraft Redelivery</li>
-            </ol>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="content">
-      <div class="container-fluid">
-        <div class="row justify-content-center">
-          <div class="col-md-12">
-            <div class="card">
-              <div class="card-body">
-                <h1>Aircraft Redelivery</h1>
-                <hr />
-
-                <form @submit.prevent="create" class="form-horizontal">
-                  <div class="card-body">
-                    <div class="row g-3 align-items-center">
-                      <div class="col-md-2">
-                        <label for="acreg" class="form-label">A/C Registration</label>
-                      </div>
-                      <div class="col-sm-4">
-                        <multiselect v-model="selected_mutation" id="ajax" :custom-label="mutationLabel" track-by="id" placeholder="Select A/C Registration" open-direction="bottom" :options="mutations" :multiple="false" :searchable="true" :loading="isLoading" :internal-search="false" :clear-on-select="false" :close-on-select="true" :options-limit="300" :max-height="600" :show-no-results="false" @search-change="findMutation" @select="selectedMutation"></multiselect>
-                        <span v-if="errors.reg" class="text-danger">{{ errors.reg[0] }}</span>
-                      </div>
+    <div>
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Aircraft Redelivery</h1>
                     </div>
-                    <br />
-
-                    <div class="form-group row">
-                      <label class="col-sm-2 col-form-label">Operator</label>
-                      <div class="col-sm-4">
-                        <input type="text" class="form-control" v-model="operator" />
-                        <span v-if="errors.operator" class="text-danger">{{ errors.operator[0] }}</span>
-                      </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item">
+                                <router-link :to="{ name: 'dashboard' }"
+                                    >Dashboard</router-link
+                                >
+                            </li>
+                            <li class="breadcrumb-item active">
+                                Aircraft Redelivery
+                            </li>
+                        </ol>
                     </div>
-                    <div class="form-group row">
-                      <label class="col-sm-2 col-form-label">A/C Type</label>
-                      <div class="col-sm-4">
-                        <input type="text" class="form-control" v-model="type" />
-                        <span v-if="errors.type" class="text-danger">{{ errors.type[0] }}</span>
-                      </div>
-                    </div>
-
-                    <div class="form-group row">
-                      <label class="col-sm-2 col-form-label">Actual Time Depart</label>
-                      <div class="col-sm-3">
-                        <input type="datetime-local" class="form-control" v-model="actual_time" />
-                        <span v-if="errors.actual_time" class="text-danger">{{
-                                                errors.actual_time[0]
-                                            }}</span>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="text-center">
-                    <button type="submit" class="btn btn-primary">
-                      Save
-                    </button>
-                    <button type="button" class="btn btn-secondary" @click="clearForm">
-                      Reset
-                    </button>
-                  </div>
-                </form>
-              </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
+
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <form
+                                    @submit.prevent="create"
+                                    class="form-horizontal"
+                                >
+                                    <div class="card-body">
+                                        <div class="row g-3 align-items-center">
+                                            <div class="col-md-2">
+                                                <label
+                                                    for="acreg"
+                                                    class="form-label"
+                                                    >A/C Registration</label
+                                                >
+                                            </div>
+                                            <div class="col-sm-4">
+                                                <multiselect
+                                                    v-model="selected_mutation"
+                                                    id="ajax"
+                                                    :custom-label="
+                                                        mutationLabel
+                                                    "
+                                                    track-by="id"
+                                                    placeholder="Select A/C Registration"
+                                                    open-direction="bottom"
+                                                    :options="mutations"
+                                                    :multiple="false"
+                                                    :searchable="true"
+                                                    :loading="isLoading"
+                                                    :internal-search="false"
+                                                    :clear-on-select="false"
+                                                    :close-on-select="true"
+                                                    :options-limit="300"
+                                                    :max-height="600"
+                                                    :show-no-results="false"
+                                                    @search-change="
+                                                        findMutation
+                                                    "
+                                                    @select="selectedMutation"
+                                                ></multiselect>
+                                                <span
+                                                    v-if="errors.reg"
+                                                    class="text-danger"
+                                                    >{{ errors.reg[0] }}</span
+                                                >
+                                            </div>
+                                        </div>
+                                        <br />
+
+                                        <div class="form-group row">
+                                            <label
+                                                class="col-sm-2 col-form-label"
+                                                >Operator</label
+                                            >
+                                            <div class="col-sm-4">
+                                                <input
+                                                    type="text"
+                                                    class="form-control"
+                                                    v-model="operator"
+                                                />
+                                                <span
+                                                    v-if="errors.operator"
+                                                    class="text-danger"
+                                                    >{{
+                                                        errors.operator[0]
+                                                    }}</span
+                                                >
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label
+                                                class="col-sm-2 col-form-label"
+                                                >A/C Type</label
+                                            >
+                                            <div class="col-sm-4">
+                                                <input
+                                                    type="text"
+                                                    class="form-control"
+                                                    v-model="type"
+                                                />
+                                                <span
+                                                    v-if="errors.type"
+                                                    class="text-danger"
+                                                    >{{ errors.type[0] }}</span
+                                                >
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label
+                                                class="col-sm-2 col-form-label"
+                                                >Actual Time Depart</label
+                                            >
+                                            <div class="col-sm-3">
+                                                <input
+                                                    type="datetime-local"
+                                                    class="form-control"
+                                                    v-model="date_out"
+                                                />
+                                                <span
+                                                    v-if="errors.date_out"
+                                                    class="text-danger"
+                                                    >{{
+                                                        errors.date_out[0]
+                                                    }}</span
+                                                >
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="text-center">
+                                        <button
+                                            type="submit"
+                                            class="btn btn-primary"
+                                        >
+                                            Save
+                                        </button>
+                                        <button
+                                            type="button"
+                                            class="btn btn-secondary"
+                                            @click="clearForm"
+                                        >
+                                            Reset
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  data() {
-    return {
-      reg: null,
-      operator: null,
-      type: null,
-      actual_time_del: null,
-      actual_time: null,
-      rksp: null,
-      status: null,
-      errors: [],
+    data() {
+        return {
+            reg: null,
+            operator: null,
+            type: null,
+            date_out: null,
+            errors: [],
 
-      mutations: [],
-      selected_mutation: null,
-      isLoading: false,
-    };
-  },
-  methods: {
-    create() {
-      axios
-        .post("/api/redelivery", {
-          reg: this.reg,
-          operator: this.operator,
-          type: this.type,
-          actual_time: this.actual_time,
-        })
-        .then((data) => {
-          this.$router.push({ name: "mutation" });
-        })
-        .catch((error) => {
-          if (error.response.status == 422) {
-            this.errors = error.response.data.errors;
-          }
-        });
+            mutations: [],
+            selected_mutation: null,
+            isLoading: false,
+        };
     },
-    clearForm() {
-      this.reg = null;
-      this.operator = null;
-      this.type = null;
-      this.actual_time = null;
-      this.status = null;
-      this.errors = [];
+    created() {
+        this.initMutation()
     },
-    findMutation(keyword) {
-      this.isLoading = true;
+    methods: {
+        create() {
+            axios
+                .post("/api/aircraft-redelivery", {
+                    reg: this.reg,
+                    operator: this.operator,
+                    type: this.type,
+                    date_out: this.date_out,
+                })
+                .then((data) => {
+                    this.$router.push({ name: "aircraft-mutation" });
+                })
+                .catch((error) => {
+                    if (error.response.status == 422) {
+                        this.errors = error.response.data.errors;
+                    }
+                });
+        },
+        clearForm() {
+            this.reg = null;
+            this.operator = null;
+            this.type = null;
+            this.date_out = null;
+            this.status = null;
+            this.errors = [];
+        },
+        initMutation() {
+            this.isLoading = true;
 
-      axios
-        .get("/api/aircraft/data", {
-          params: {
-            search: keyword,
-          },
-        })
-        .then((response) => {
-          this.mutations = response.data;
-          this.isLoading = false;
-        });
+            axios
+                .get("/api/aircraft/data")
+                .then((response) => {
+                    this.mutations = response.data;
+                    this.isLoading = false;
+                });
+        },
+        findMutation(keyword) {
+            this.isLoading = true;
+
+            axios
+                .get("/api/aircraft/data", {
+                    params: {
+                        search: keyword,
+                    },
+                })
+                .then((response) => {
+                    this.mutations = response.data;
+                    this.isLoading = false;
+                });
+        },
+        mutationLabel({ reg }) {
+            return `${reg}`;
+        },
+        selectedMutation({ reg, operator, type }) {
+            this.reg = reg;
+            this.operator = operator;
+            this.type = type;
+        },
     },
-    mutationLabel({ reg }) {
-      return `${reg}`;
-    },
-    selectedMutation({ reg, operator, type }) {
-      this.reg = reg
-      this.operator = operator
-      this.type = type
-    },
-  },
 };
 </script>
