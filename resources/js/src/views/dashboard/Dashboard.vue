5c4d4b1bf1a2a089788cdf55927d1ca69fbcb0c5
Galuh Idam <galuh.danutirto@opencloud.id>
2022-06-14 17:57:04 +0700

    Membuat UI Dashboard (Masih perlu disempurnakan)

--- resources/js/src/views/dashboard/Dashboard.vue
+++ resources/js/src/views/dashboard/Dashboard.vue
@@ -1,11 +1,10 @@
-
 <template>
   <div>
     <div class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Home</h1>
+            <h1 class="m-0">Dashboard</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -21,75 +20,237 @@
       <div class="container-fluid">
         <div class="row">
           <!-- START: Report Inbound -->
-          <div class="col-lg-4 col-6">
-            <div class="small-box bg-info">
-              <div class="inner">
-                <h4>Report</h4>
-                <h5>Inbound</h5>
-              </div>
-              <div class="icon">
-                <i class="fa-solid fa-file-arrow-down"></i>
-              </div>
-              <a href="#" class="small-box-footer">View Detail <i class="fas fa-arrow-circle-right"></i></a>
+          <div class="col-lg-6">
+            <div class="medium-box bg-white">
+              <div id="chartSatu"></div>
             </div>
           </div>
-
-          <!-- START: Report Outbound -->
-          <div class="col-lg-4 col-6">
-            <div class="small-box bg-danger">
-              <div class="inner">
-                <h4>Report</h4>
-                <h5>Outbound</h5>
-              </div>
-              <div class="icon">
-                <i class="fa-solid fa-file-arrow-up"></i>
-              </div>
-              <a href="#" class="small-box-footer">View Detail <i class="fas fa-arrow-circle-right"></i></a>
+          <div class="col-lg-6">
+            <div class="medium-box bg-white">
+              <div id="chartDua"></div>
             </div>
           </div>
-          <!-- END: Report Outbound -->
-
-          <!-- START: Report Mutasi -->
-          <div class="col-lg-4 col-6">
-            <div class="small-box bg-warning">
-              <div class="inner">
-                <h4>Report</h4>
-                <h5>Mutasi</h5>
-              </div>
-              <div class="icon">
-                <i class="fa-solid fa-file-circle-exclamation"></i>
-              </div>
-              <a href="#" class="small-box-footer">View Detail <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- END: Report Mutasi -->
         </div>
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="card">
-              <div class="card-header text-center bg-success">
-                <div class="d-flex align-items-center">
-                  <h5 class="mx-auto w-100">Coming Soon</h5>
-                </div>
-              </div>
-              <div class="card-body">
-                <h2 class="text-center">Under Devleopment</h2>
-                <h1 class="text-center"><i class="fa-solid fa-laptop-code"></i></h1>
-              </div>
+        <div class="row d-flex justify-content-center mt-3">
+          <!-- START: Report Inbound -->
+          <div class="col-lg-6">
+            <div class="medium-box bg-white">
+              <div id="pieSatu"></div>
             </div>
-
+          </div>
+        </div>
+        <div class="row mt-3">
+          <!-- START: Report Inbound -->
+          <div class="col-lg-6">
+            <div class="medium-box bg-white">
+              <div id="bar"></div>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="medium-box bg-white">
+              <div id="pieDua"></div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
+import ApexCharts from "apexcharts";
+function cartSatu() {
+  var options = {
+    series: [
+      {
+        name: "Data Satu",
+        type: "column",
+        data: [587, 778, 6687],
+      },
+      {
+        name: "Data Dua",
+        type: "line",
+        data: [null, 1268, 5214],
+      },
+    ],
+    chart: {
+      height: 350,
+      type: "line",
+    },
+    stroke: {
+      width: [0, 4],
+    },
+    title: {
+      text: "Status Part Consume Install Periode",
+    },
+    dataLabels: {
+      enabled: true,
+      enabledOnSeries: [1],
+    },
+    labels: ["Outbound Satu", "Outbound Dua", "Outbound Tiga"],
+    xaxis: {
+      type: "text",
+    },
+    yaxis: [
+      {
+        title: {
+          text: "Hasil Satu",
+        },
+      },
+      {
+        opposite: true,
+        title: {
+          text: "Hasil Dua",
+        },
+      },
+    ],
+  };
+  var chart = new ApexCharts(document.querySelector("#chartSatu"), options);
+  chart.render();
+}
+function cartDua() {
+  var options = {
+    colors: ["#FF5733", "#DBFF33"],
+    series: [
+      {
+        name: "Data Satu",
+        type: "column",
+        data: [587, 778, 6687],
+      },
+      {
+        name: "Data Dua",
+        type: "line",
+        data: [300, 1268, 5214],
+      },
+    ],
+    chart: {
+      height: 350,
+      type: "line",
+    },
+    stroke: {
+      width: [0, 4],
+    },
+    title: {
+      text: "Status BC 28 Tahun 2022",
+    },
+    dataLabels: {
+      enabled: true,
+      enabledOnSeries: [1],
+    },
+    labels: ["Januari", "Februari", "Maret"],
+    xaxis: {
+      type: "text",
+    },
+    yaxis: [
+      {
+        title: {
+          text: "Hasil Satu",
+        },
+      },
+      {
+        opposite: true,
+        title: {
+          text: "Hasil Dua",
+        },
+      },
+    ],
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chartDua"), options);
+  chart.render();
+}
+
+function pieSatu() {
+  var options = {
+    series: [96, 4],
+
+    chart: {
+      width: 380,
+      type: "pie",
+    },
+    title: {
+      text: "Status Masa Timbun BC 16",
+    },
+    labels: ["BC 16 < 3 Tahun", "BC 16 > 3 Tahun"],
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200,
+          },
+          legend: {
+            position: "bottom",
+          },
+        },
+      },
+    ],
+  };
+
+  var chart = new ApexCharts(document.querySelector("#pieSatu"), options);
+  chart.render();
+}
+function pieDua() {
+  var options = {
+    series: [2, 10, 4, 2, 2],
+
+    chart: {
+      width: 380,
+      type: "pie",
+    },
+    title: {
+      text: "Pesawat yang ada di Hangar PT GMF AeroAsia",
+    },
+    labels: ["Customer", "Garuda", "Citilink", "Indigo", "KLM"],
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200,
+          },
+          legend: {
+            position: "bottom",
+          },
+        },
+      },
+    ],
+  };
+
+  var chart = new ApexCharts(document.querySelector("#pieDua"), options);
+  chart.render();
+}
+
+function bar() {
+  var options = {
+    title: {
+      text: "Total QTY OSA Allotment",
+    },
+    chart: {
+      type: "bar",
+    },
+    series: [
+      {
+        name: "sales",
+        data: [305, 1054, 768, 844, 945, 658],
+      },
+    ],
+    xaxis: {
+      categories: ["BPN", "CGK", "DPS", "KNO", "SUB", "UPG"],
+    },
+  };
+
+  var chart = new ApexCharts(document.querySelector("#bar"), options);
+
+  chart.render();
+}
 export default {
   mounted() {
-    console.log('Component mounted.')
-  }
-}
+    cartSatu();
+    cartDua();
+    pieSatu();
+    pieDua();
+    bar();
+  },
+};
 </script>
