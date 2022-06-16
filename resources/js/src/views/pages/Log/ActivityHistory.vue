19c376c0199a7aec99814ac933a1c728f1ed39de
Jacob Ginting <jacob.ginting@opencloud.id>
2022-06-16 09:29:19 +0700

    UI untuk activity history

--- resources/js/src/views/pages/Log/ActivityHistory.vue
+++ resources/js/src/views/pages/Log/ActivityHistory.vue
@@ -4,12 +4,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Activity Histroy</h1>
+            <h1 class="m-0">Activity History</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
-              <li class="breadcrumb-item active"><a href="/activity-history">Activity Histroy</a></li>
+              <li class="breadcrumb-item active"><a href="/activity-history">Activity History</a></li>
             </ol>
           </div>
         </div>
@@ -21,10 +21,103 @@
         <div class="row justify-content-center">
           <div class="col-md-12">
             <div class="card">
-              <div class="card-header">Activity Histroy</div>
+              <div class="card-header">Activity History</div>
+              <tr>
+                <div class="row d-flex align-items-center mt-2 mb-2">
+                  <div class="col-sm-12 ml-3 col-md-6">
+                    <div class="dt-buttons btn-group flex-wrap">
+                      <button class="btn btn-secondary buttons-copy buttons-html5" tabindex="0" aria-controls="example1" type="button"><span>Copy</span></button>
+                      <button class="btn btn-secondary buttons-csv buttons-html5" tabindex="0" aria-controls="example1" type="button"><span>CSV</span></button>
+                      <button class="btn btn-secondary buttons-excel buttons-html5" tabindex="0" aria-controls="example1" type="button"><span>Excel</span></button>
+                      <button class="btn btn-secondary buttons-pdf buttons-html5" tabindex="0" aria-controls="example1" type="button"><span>PDF</span></button>
+                      <button class="btn btn-secondary buttons-print" tabindex="0" aria-controls="example1" type="button"><span>Print</span></button>
+                      <div class="btn-group">
+                        <button class="btn btn-secondary buttons-collection dropdown-toggle buttons-colvis" tabindex="0" aria-controls="example1" type="button" aria-haspopup="true">
+                          <span>Column visibility</span><span class="dt-down-arrow"></span>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col d-flex justify-content-end">
+                    <input type="text" class="form-control-sm mt-0 mr-3" placeholder="Search:.." aria-label="Recipient's username" aria-describedby="button-addon2" />
+                  </div>
+                </div>
+              </tr>
 
               <div class="card-body">
-                Tampilkan coba 
+                <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                  <div class="row"></div>
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <table id="example1" class="table table-bordered table-striped dataTable dtr-inline" aria-describedby="example1_info">
+                        <thead>
+                          <tr>
+                            <th class="sorting sorting_asc" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="number: activate to sort column descending">No</th>
+                            <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Username: activate to sort column ascending">Username</th>
+                            <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Time: activate to sort column ascending">Time</th>
+                            <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Ip.Address: activate to sort column ascending">Activity</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr class="odd">
+                            <td class="dtr-control sorting_1" tabindex="0">1</td>
+                            <td>tmb_gmf</td>
+                            <td>2018-09-17 11:25:21</td>
+                            <td>Akses Pemasukan Barang</td>
+                          </tr>
+                          <tr class="even">
+                            <td class="dtr-control sorting_1" tabindex="0">2</td>
+                            <td>tgo_gmf</td>
+                            <td>2018-09-17 11:25:48 ​</td>
+                            <td>Akses Pemasukan Barang</td>
+                          </tr>
+                          <tr class="even">
+                            <td class="dtr-control sorting_1" tabindex="0">2</td>
+                            <td>tgo_gmf</td>
+                            <td>2018-09-17 11:26:11 ​</td>
+                            <td>Akses Pengeluaran Barang</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="d-flex flex-row bd-highlight">
+                      <div class="col ml-0">
+                        <div class="btn-group">
+                          <button type="button" class="btn btn-info">10</button>
+                          <button type="button" class="btn btn-info dropdown-toggle dropdown-icon" data-toggle="dropdown">
+                            <span class="sr-only">Toggle Dropdown</span>
+                          </button>
+                          <div class="dropdown-menu" role="menu">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Separated link</a>
+                          </div>
+                          <div class="d-flex align-items-center" style="width: fit-content">
+                            <div>Tampilkan entries</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col d-flex justify-content-end col-ml-12">
+                      <div class="dataTables_paginate paging_simple_numbers" id="example1_paginate">
+                        <ul class="pagination">
+                          <li class="paginate_button page-item previous disabled" id="example1_previous"><a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li>
+                          <li class="paginate_button page-item active"><a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
+                          <li class="paginate_button page-item"><a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
+                          <li class="paginate_button page-item"><a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0" class="page-link">3</a></li>
+                          <li class="paginate_button page-item"><a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0" class="page-link">4</a></li>
+                          <li class="paginate_button page-item"><a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0" class="page-link">5</a></li>
+                          <li class="paginate_button page-item"><a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0" class="page-link">6</a></li>
+                          <li class="paginate_button page-item next" id="example1_next"><a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
