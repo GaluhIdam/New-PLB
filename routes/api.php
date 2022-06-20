0af0a29993ff33b9ecd52a0b107e7b5889d8afb7
Fatkhur Ridlwan <fridlwan@gmail.com>
2022-06-20 13:56:20 +0700

    Fix conflict

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,15 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Routing\RouteGroup;
 use Illuminate\Support\Facades\Route;
-use App\Http\Traits\ActivityHistoryTrait;
 use App\Http\Controllers\CustomsController;
 use App\Http\Controllers\AircraftController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\API\UserController;
-<<<<<<< HEAD
-=======
 use App\Http\Controllers\LoginHistoryController;
 use App\Http\Controllers\MutationController;
 use App\Http\Controllers\MutationPeriodicController;
->>>>>>> 3faa9dbe2c7e15a4c652f3e8e63e6f2fed6457e1
 use App\Http\Controllers\OutboundController;
-use App\Http\Controllers\LoginHistoryController;
 use App\Http\Controllers\ActivityHistoryController;
 
 // Authentication routes
@@ -29,14 +23,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 // Routing User 
 Route::apiResource('user', UserController::class);
-Route::get('profile', [UserController::class, 'profile']);
-Route::put('profile', [UserController::class, 'updateProfile']);
 
-<<<<<<< HEAD
-
-=======
 // Dokumen Kepabeanan
->>>>>>> 3faa9dbe2c7e15a4c652f3e8e63e6f2fed6457e1
 Route::get('/customs/inbound-document', [CustomsController::class, 'inboundDocument']);
 Route::get('/customs/outbound-document', [CustomsController::class, 'outboundDocument']);
 
@@ -55,15 +43,12 @@ Route::get('/aircraft/data', [AircraftController::class, 'data']);
 Route::get('/login-history', [LoginHistoryController::class, 'index']);
 Route::get('/login-history/{id}', [LoginHistoryController::class, 'index']);
 
-<<<<<<< HEAD
 //Activity History
 Route::get('/activity-history', [ActivityHistoryController::class, 'index']);
-Route::post('/activity-history', [ActivityHistoryTrait::class, 'recordActivity']);
-=======
+
 // Mutations
 Route::get('/mutation', [MutationController::class, 'index']);
 
 // Periodic Mutations
 Route::get('/mutation-periodic', [MutationPeriodicController::class, 'index']);
 Route::delete('/mutation-periodic/{id}', [MutationPeriodicController::class, 'destroy']);
->>>>>>> 3faa9dbe2c7e15a4c652f3e8e63e6f2fed6457e1
