988bc1ee8ff0f3129c4d0af2a61122802226d4cd
Fatkhur Ridlwan <fridlwan@gmail.com>
2022-06-20 13:19:40 +0700

    Update api

--- app/Models/CustomsInboundDocument.php
+++ app/Models/CustomsInboundDocument.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class CustomsInboundDocument extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'document_type',
+        'no_aju',
+        'date_aju',
+        'no_register',
+        'date_register',
+        'date_inbound',
+        'sender',
+        'owner',
+        'item_code',
+        'hs_code',
+        'item_name',
+        'quantity',
+        'unit',
+        'item_value',
+        'attachment',
+    ];
 }
--- app/Models/CustomsOutboundDocument.php
+++ app/Models/CustomsOutboundDocument.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class CustomsOutboundDocument extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'document_type',
+        'no_aju',
+        'date_aju',
+        'no_register',
+        'date_register',
+        'date_outbound',
+        'owner',
+        'item_code',
+        'hs_code',
+        'item_name',
+        'quantity',
+        'unit',
+        'item_value',
+        'attachment',
+    ];
 }
