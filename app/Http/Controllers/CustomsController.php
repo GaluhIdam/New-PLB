d70d23968f06e1c38143f4b525d1c3a9e1ec763f
vanadia22 <vanadia.equila@opencloud.id>
2022-06-30 10:34:43 +0700

    Menambah Fungsi Filter dan Reset- Dokumen Kepabeanan- Pengeluaran PLB

--- app/Http/Controllers/CustomsController.php
+++ app/Http/Controllers/CustomsController.php
@@ -16,7 +16,26 @@ class CustomsController extends Controller
     public function inboundDocument(Request $request)
     {
         $search = $request->get('search');
-        
+        $search_document_type = $request->get('search_document_type');
+        $search_no_aju = $request->get('search_no_aju');
+        $search_date_aju = $request->get('search_date_aju');
+        $search_no_register = $request->get('search_no_register');
+        $search_date_register = $request->get('search_date_register');
+        $search_date_inbound = $request->get('search_date_inbound');
+        $search_sender = $request->get('search_sender');
+        $search_owner = $request->get('search_owner');
+        $search_item_code = $request->get('search_item_code');
+        $search_hs_code = $request->get('search_hs_code');
+        $search_item_name = $request->get('search_item_name');
+        $search_quantity = $request->get('search_quantity');
+        $search_unit = $request->get('search_unit');
+        $search_item_value = $request->get('search_item_value');
+
+        $filter_start_date = $request->get('filter_start_date');
+        $filter_end_date = $request->get('filter_end_date');
+        $filter_no_aju = $request->get('filter_no_aju');
+        $filter_document_type = $request->get('filter_document_type');
+
         if ($request->get('order') && $request->get('by')) {
             $order = $request->get('order');
             $by = $request->get('by');
@@ -25,22 +44,85 @@ class CustomsController extends Controller
             $by = 'desc';
         }
 
+        if ($request->get('paginate')) {
+            $paginate = $request->get('paginate');
+        } else {
+            $paginate = 10;
+        }
+
         $inbounds = CustomsInboundDocument::when($search, function ($query) use ($search) {
-                $query->where(function ($sub_query) use ($search) {
-                    $sub_query->where('document_type', 'LIKE', "%{$search}%")
-                        ->orWhere('no_aju', 'LIKE', "%{$search}%")
-                        ->orWhere('no_register', 'LIKE', "%{$search}%")
-                        ->orWhere('sender', 'LIKE', "%{$search}%")
-                        ->orWhere('owner', 'LIKE', "%{$search}%")
-                        ->orWhere('item_code', 'LIKE', "%{$search}%")
-                        ->orWhere('hs_code', 'LIKE', "%{$search}%")
-                        ->orWhere('item_name', 'LIKE', "%{$search}%");
-                });
+            $query->where(function ($sub_query) use ($search) {
+                $sub_query->where('document_type', 'LIKE', "%{$search}%")
+                    ->orWhere('no_aju', 'LIKE', "%{$search}%")
+                    ->orWhere('no_register', 'LIKE', "%{$search}%")
+                    ->orWhere('sender', 'LIKE', "%{$search}%")
+                    ->orWhere('owner', 'LIKE', "%{$search}%")
+                    ->orWhere('item_code', 'LIKE', "%{$search}%")
+                    ->orWhere('hs_code', 'LIKE', "%{$search}%")
+                    ->orWhere('item_name', 'LIKE', "%{$search}%")
+                    ->orWhere('quantity', 'LIKE', "%{$search}%")
+                    ->orWhere('unit', 'LIKE', "%{$search}%")
+                    ->orWhere('item_value', 'LIKE', "%{$search}%");
+            });
+        })
+            ->when($search_document_type, function ($query) use ($search_document_type) {
+                $query->where('document_type', 'LIKE', "%{$search_document_type}%");
+            })
+            ->when($search_no_aju, function ($query) use ($search_no_aju) {
+                $query->where('no_aju', 'LIKE', "%{$search_no_aju}%");
+            })
+            ->when($search_date_aju, function ($query) use ($search_date_aju) {
+                $query->whereDate('date_aju', $search_date_aju);
+            })
+            ->when($search_no_register, function ($query) use ($search_no_register) {
+                $query->where('no_register', 'LIKE', "%{$search_no_register}%");
+            })
+            ->when($search_date_register, function ($query) use ($search_date_register) {
+                $query->whereDate('date_register', $search_date_register);
+            })
+            ->when($search_date_inbound, function ($query) use ($search_date_inbound) {
+                $query->whereDate('date_inbound', $search_date_inbound);
+            })
+            ->when($search_sender, function ($query) use ($search_sender) {
+                $query->where('sender', 'LIKE', "%{$search_sender}%");
+            })
+            ->when($search_owner, function ($query) use ($search_owner) {
+                $query->where('owner', 'LIKE', "%{$search_owner}%");
+            })
+            ->when($search_item_code, function ($query) use ($search_item_code) {
+                $query->where('item_code', 'LIKE', "%{$search_item_code}%");
+            })
+            ->when($search_hs_code, function ($query) use ($search_hs_code) {
+                $query->where('hs_code', 'LIKE', "%{$search_hs_code}%");
+            })
+            ->when($search_item_name, function ($query) use ($search_item_name) {
+                $query->where('item_name', 'LIKE', "%{$search_item_name}%");
+            })
+            ->when($search_quantity, function ($query) use ($search_quantity) {
+                $query->where('quantity', 'LIKE', "%{$search_quantity}%");
+            })
+            ->when($search_unit, function ($query) use ($search_unit) {
+                $query->where('unit', 'LIKE', "%{$search_unit}%");
+            })
+            ->when($search_item_value, function ($query) use ($search_item_value) {
+                $query->where('item_value', 'LIKE', "%{$search_item_value}%");
             })
             ->when(($order && $by), function ($query) use ($order, $by) {
                 $query->orderBy($order, $by);
             })
-            ->paginate(10);
+            ->when($filter_start_date, function ($query) use ($filter_start_date) {
+                $query->whereDate('date_aju', '>=', $filter_start_date);
+            })
+            ->when($filter_end_date, function ($query) use ($filter_end_date) {
+                $query->whereDate('date_aju', '<=', $filter_end_date);
+            })
+            ->when($filter_no_aju, function ($query) use ($filter_no_aju) {
+                $query->where('no_aju', 'LIKE', "%{$filter_no_aju}%");
+            })
+            ->when($filter_document_type, function ($query) use ($filter_document_type) {
+                $query->whereIn('document_type', $filter_document_type);
+            })
+            ->paginate($paginate);
 
         $query_string = [
             'search' => $search,
@@ -56,7 +138,25 @@ class CustomsController extends Controller
     public function outboundDocument(Request $request)
     {
         $search = $request->get('search');
-        
+        $search_document_type = $request->get('search_document_type');
+        $search_no_aju = $request->get('search_no_aju');
+        $search_date_aju = $request->get('search_date_aju');
+        $search_no_register = $request->get('search_no_register');
+        $search_date_register = $request->get('search_date_register');
+        $search_date_outbound = $request->get('search_date_outbound');
+        $search_owner = $request->get('search_owner');
+        $search_item_code = $request->get('search_item_code');
+        $search_hs_code = $request->get('search_hs_code');
+        $search_item_name = $request->get('search_item_name');
+        $search_quantity = $request->get('search_quantity');
+        $search_unit = $request->get('search_unit');
+        $search_item_value = $request->get('search_item_value');
+
+        $filter_start_date = $request->get('filter_start_date');
+        $filter_end_date = $request->get('filter_end_date');
+        $filter_no_aju = $request->get('filter_no_aju');
+        $filter_document_type = $request->get('filter_document_type');
+
         if ($request->get('order') && $request->get('by')) {
             $order = $request->get('order');
             $by = $request->get('by');
@@ -65,30 +165,86 @@ class CustomsController extends Controller
             $by = 'desc';
         }
 
-        $inbounds = CustomsOutboundDocument::when($search, function ($query) use ($search) {
-                $query->where(function ($sub_query) use ($search) {
-                    $sub_query->where('document_type', 'LIKE', "%{$search}%")
-                        ->orWhere('no_aju', 'LIKE', "%{$search}%")
-                        ->orWhere('no_register', 'LIKE', "%{$search}%")
-                        ->orWhere('owner', 'LIKE', "%{$search}%")
-                        ->orWhere('item_code', 'LIKE', "%{$search}%")
-                        ->orWhere('hs_code', 'LIKE', "%{$search}%")
-                        ->orWhere('item_name', 'LIKE', "%{$search}%");
-                });
+        if ($request->get('paginate')) {
+            $paginate = $request->get('paginate');
+        } else {
+            $paginate = 10;
+        }
+
+        $outbounds = CustomsOutboundDocument::when($search, function ($query) use ($search) {
+            $query->where(function ($sub_query) use ($search) {
+                $sub_query->where('document_type', 'LIKE', "%{$search}%")
+                    ->orWhere('no_aju', 'LIKE', "%{$search}%")
+                    ->orWhere('no_register', 'LIKE', "%{$search}%")
+                    ->orWhere('owner', 'LIKE', "%{$search}%")
+                    ->orWhere('item_code', 'LIKE', "%{$search}%")
+                    ->orWhere('hs_code', 'LIKE', "%{$search}%")
+                    ->orWhere('item_name', 'LIKE', "%{$search}%")
+                    ->orWhere('quantity', 'LIKE', "%{$search}%")
+                    ->orWhere('unit', 'LIKE', "%{$search}%")
+                    ->orWhere('item_value', 'LIKE', "%{$search}%");
+            });
+        })
+            ->when($search_document_type, function ($query) use ($search_document_type) {
+                $query->where('document_type', 'LIKE', "%{$search_document_type}%");
+            })
+            ->when($search_no_aju, function ($query) use ($search_no_aju) {
+                $query->where('no_aju', 'LIKE', "%{$search_no_aju}%");
+            })
+            ->when($search_date_aju, function ($query) use ($search_date_aju) {
+                $query->whereDate('date_aju', $search_date_aju);
+            })
+            ->when($search_no_register, function ($query) use ($search_no_register) {
+                $query->where('no_register', 'LIKE', "%{$search_no_register}%");
+            })
+            ->when($search_date_register, function ($query) use ($search_date_register) {
+                $query->whereDate('date_register', $search_date_register);
+            })
+            ->when($search_date_outbound, function ($query) use ($search_date_outbound) {
+                $query->whereDate('date_outbound', $search_date_outbound);
+            })
+            ->when($search_owner, function ($query) use ($search_owner) {
+                $query->where('owner', 'LIKE', "%{$search_owner}%");
+            })
+            ->when($search_item_code, function ($query) use ($search_item_code) {
+                $query->where('item_code', 'LIKE', "%{$search_item_code}%");
+            })
+            ->when($search_hs_code, function ($query) use ($search_hs_code) {
+                $query->where('hs_code', 'LIKE', "%{$search_hs_code}%");
+            })
+            ->when($search_item_name, function ($query) use ($search_item_name) {
+                $query->where('item_name', 'LIKE', "%{$search_item_name}%");
+            })
+            ->when($search_quantity, function ($query) use ($search_quantity) {
+                $query->where('quantity', 'LIKE', "%{$search_quantity}%");
+            })
+            ->when($search_unit, function ($query) use ($search_unit) {
+                $query->where('unit', 'LIKE', "%{$search_unit}%");
+            })
+            ->when($search_item_value, function ($query) use ($search_item_value) {
+                $query->where('item_value', 'LIKE', "%{$search_item_value}%");
+            })
+            ->when($filter_start_date, function ($query) use ($filter_start_date) {
+                $query->whereDate('date_aju', '>=', $filter_start_date);
+            })
+            ->when($filter_end_date, function ($query) use ($filter_end_date) {
+                $query->whereDate('date_aju', '<=', $filter_end_date);
+            })
+            ->when($filter_no_aju, function ($query) use ($filter_no_aju) {
+                $query->where('no_aju', 'LIKE', "%{$filter_no_aju}%");
+            })
+            ->when($filter_document_type, function ($query) use ($filter_document_type) {
+                $query->whereIn('document_type', $filter_document_type);
             })
             ->when(($order && $by), function ($query) use ($order, $by) {
                 $query->orderBy($order, $by);
             })
-            ->paginate(10);
+            ->paginate($paginate);
 
         $query_string = [
             'search' => $search,
             'order' => $order,
             'by' => $by,
         ];
-
-        $inbounds->appends($query_string);
-
-        return $inbounds;
     }
 }
--- database/seeders/CustomsOutboundDocumentSeeder.php
+++ database/seeders/CustomsOutboundDocumentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use App\Models\CustomsOutboundDocument;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -18,10 +19,10 @@ class CustomsOutboundDocumentSeeder extends Seeder
         CustomsOutboundDocument::create([
             'document_type' => '16',
             'no_aju' => '1122330558620210822018786',
-            'date_aju' => '2020-05-05',
+            'date_aju' => Carbon::now(),
             'no_register' => '000643',
-            'date_register' => '2020-03-05',
-            'date_outbound' => '2020-05-15',
+            'date_register' => Carbon::now(),
+            'date_outbound' => Carbon::now(),
             'owner' => 'VANTURE',
             'item_code' => '6240-35:C5307',
             'hs_code' => 'CX342',
@@ -35,10 +36,10 @@ class CustomsOutboundDocumentSeeder extends Seeder
         CustomsOutboundDocument::create([
             'document_type' => '41',
             'no_aju' => '05014100558620210822018786',
-            'date_aju' => '2021-08-22',
+            'date_aju' => Carbon::now()->addDays(1),
             'no_register' => '000643',
-            'date_register' => '2021-08-22',
-            'date_outbound' => '2021-08-22',
+            'date_register' => Carbon::now()->addDays(1),
+            'date_outbound' => Carbon::now()->addDays(1),
             'owner' => 'GARUDA',
             'item_code' => '6240-35:C5307',
             'hs_code' => 'CX342',
@@ -52,10 +53,10 @@ class CustomsOutboundDocumentSeeder extends Seeder
         CustomsOutboundDocument::create([
             'document_type' => '41',
             'no_aju' => '09084100558620210822018786',
-            'date_aju' => '2021-08-22',
+            'date_aju' => Carbon::now()->addDays(2),
             'no_register' => '000643',
-            'date_register' => '2021-08-22',
-            'date_outbound' => '2021-08-22',
+            'date_register' => Carbon::now()->addDays(2),
+            'date_outbound' => Carbon::now()->addDays(2),
             'owner' => 'SENTRA',
             'item_code' => '6240-35:C5307',
             'hs_code' => 'CX342',
@@ -69,10 +70,10 @@ class CustomsOutboundDocumentSeeder extends Seeder
         CustomsOutboundDocument::create([
             'document_type' => '41',
             'no_aju' => '05614100558620210822018786',
-            'date_aju' => '2021-08-22',
+            'date_aju' => Carbon::now()->addDays(3),
             'no_register' => '000643',
-            'date_register' => '2021-08-22',
-            'date_outbound' => '2021-08-22',
+            'date_register' => Carbon::now()->addDays(3),
+            'date_outbound' => Carbon::now()->addDays(3),
             'owner' => 'MEDIKA',
             'item_code' => '6240-35:C5307',
             'hs_code' => 'CX342',
