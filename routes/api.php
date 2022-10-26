f526296400a52c00ddf7cb6677a26a7970b2e230
Rizky Ibrahim <rizky.ibrahim@opencloud.id>
2022-10-26 13:02:42 +0700

    [Fixing] Route API

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AircraftController;
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\API\TestingController;
