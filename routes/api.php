4bbd72786df2a4cdac52e7d2b23a63c3dc4915fc
Rizky Ibrahim <rizky.ibrahim@opencloud.id>
2022-11-16 13:17:34 +0700

    [Fixing] Fixing Delete Button Aircraft Muatation

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,26 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\UserController;
-use App\Http\Controllers\API\DashboardController;
 use App\Http\Controllers\HoardingTimeController;
-use App\Http\Controllers\LoginHistoryController;
 use App\Http\Controllers\MutationReportController;
-use App\Http\Controllers\ActivityHistoryController;
 use App\Http\Controllers\MutationPeriodicController;
 use App\Http\Controllers\MovementAllotmentController;
 
-// Module Aircraft
+// Authenticated Controller
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\UserController;
+// Login History & Activity Log Controller 
+use App\Http\Controllers\ActivityHistoryController;
+use App\Http\Controllers\LoginHistoryController;
+// Dashboard Controller
+use App\Http\Controllers\API\DashboardController;
+// Module Aircraft Controller
 use App\Http\Controllers\API\Aircraft\AircraftController;
-// Dokumen Kepabeanan
+// Dokumen Kepabeanan Controller 
 use App\Http\Controllers\API\Costums\InboundController;
 use App\Http\Controllers\API\Costums\OutboundController;
 use App\Http\Controllers\API\Allotment\InventoryController;
-// Outbound
+// Outbound Controller
 use App\Http\Controllers\API\Outbound\OutboundOneController;
 use App\Http\Controllers\API\Outbound\OutboundSummaryController;
 use App\Http\Controllers\API\Outbound\OutboundTwoController;
@@ -27,7 +30,6 @@ use App\Http\Controllers\API\Outbound\OutboundThreeController;
 // Authentication routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
@@ -40,12 +42,11 @@ Route::apiResource('user', UserController::class);
 Route::get('/customs/inbound', [InboundController::class, 'index']); // Dokumen Kepabeanan Pemasukan (Inbound)
 Route::get('/customs/inbound-csv', [InboundController::class, 'exportCsv']); // Dokumen Kepabeanan Pemasukan (Inbound) - Export CSV
 Route::get('/customs/inbound-excel', [InboundController::class, 'exportExcel']); // Dokumen Kepabeanan Pemasukan (Inbound) - Export Excel
-
 Route::get('/customs/outbound', [OutboundController::class, 'index']); // Dokumen Kepabeanan Pengeluaran (Outbound)
 Route::get('/customs/outbound-csv', [OutboundController::class, 'exportCsv']); // Dokumen Kepabeanan Pengeluaran (Outbound) - Outbound - Export CSV
 Route::get('/customs/outbound-excel', [OutboundController::class, 'exportExcel']); // Dokumen Kepabeanan Pengeluaran (Outbound) - Outbound - Export Excel
 
-// Outbound
+// Outbound Transaction
 Route::get('/outbound-transaction-1', [OutboundOneController::class, 'index']); // Outbound Transaction 1
 Route::get('/outbound-transaction-1-csv', [OutboundOneController::class, 'exportCsv']); // Outbound Transaction 1 - Export CSV
 Route::get('/outbound-transaction-1-excel', [OutboundOneController::class, 'exportExcel']); // Outbound Transaction 1 - Export Excel
@@ -55,6 +56,7 @@ Route::get('/outbound-transaction-2-excel', [OutboundTwoController::class, 'expo
 Route::get('/outbound-transaction-3', [OutboundThreeController::class, 'index']); // Outbound Transaction 3
 Route::get('/outbound-transaction-3-csv', [OutboundThreeController::class, 'exportCsv']); // Outbound Transaction 3 - Export CSV
 Route::get('/outbound-transaction-3-excel', [OutboundThreeController::class, 'exportExcel']); // Outbound Transaction 3 - Export Excel
+// Outbound Summary // TODO : Will be Development soon
 Route::get('/outbound-summary', [OutboundSummaryController::class, 'index']); // Outbound Summary
 Route::get('/outbound-summary-csv', [OutboundSummaryController::class, 'exportCsv']); // Outbound Summary - Export CSV
 Route::get('/outbound-summary-excel', [OutboundSummaryController::class, 'exportExcel']); // Outbound Summary - Export Excel
@@ -67,30 +69,28 @@ Route::get('/aircraft-mutation-excel', [AircraftController::class, 'exportExcel'
 Route::get('/aircraft-data', [AircraftController::class, 'store']); // Aircraft Data
 Route::post('/aircraft-delivery', [AircraftController::class, 'createDelivery']); // Aircraft Delivery
 Route::post('/aircraft-redelivery', [AircraftController::class, 'createRedelivery']); // Aircraft Redivery
-Route::delete('/aircraft/{id}', [AircraftController::class, 'deleteDelivery']); //Delete Aircraft Data
+Route::delete('/aircraft/{id}', [AircraftController::class, 'destroy']); //Delete Aircraft Data
 
-// Log -> Login History
+// Login History & Activity History
 Route::get('/login-history', [LoginHistoryController::class, 'index']);
 Route::get('/login-history/{id}', [LoginHistoryController::class, 'index']);
-
-//Log - > Activity History
 Route::get('/activity-history', [ActivityHistoryController::class, 'index']);
 
-// Mutasi - > (Report Mutasi)
-Route::get('/mutation-report', [MutationReportController::class, 'index']);
-
-// Mutasi -> Periodic Mutations (Report Mutasi Berkala)
-Route::get('/mutation-periodic', [MutationPeriodicController::class, 'index']);
-Route::post('/mutation-periodic-report', [MutationPeriodicController::class, 'searchDate']);
-
-// Masa Timbun (Hoarding Time)
-Route::get('/hoarding-time', [HoardingTimeController::class, 'index']);
-
-// Allotment
-Route::get('inventory-allotment', [InventoryController::class, 'inventoryAllotment']);
-Route::get('movement-allotment', [MovementAllotmentController::class, 'index']);
-
-//Dashboard
+// Dashboard
 Route::get('dashboard-one', [DashboardController::class, 'chartOne']);
 Route::get('dashboard-two', [DashboardController::class, 'chartTwo']);
 Route::get('dashboard-three', [DashboardController::class, 'chartThree']);
+
+// Mutasi - > (Report Mutasi) // TODO : Will be Development soon
+Route::get('/mutation-report', [MutationReportController::class, 'index']);
+
+// Mutasi -> Periodic Mutations (Report Mutasi Berkala) // TODO : Will be Development soon
+Route::get('/mutation-periodic', [MutationPeriodicController::class, 'index']);
+Route::post('/mutation-periodic-report', [MutationPeriodicController::class, 'searchDate']);
+
+// Masa Timbun (Hoarding Time) // TODO : Will be Development soon
+Route::get('/hoarding-time', [HoardingTimeController::class, 'index']);
+
+// Allotment // TODO : Will be Development soon
+Route::get('inventory-allotment', [InventoryController::class, 'inventoryAllotment']);
+Route::get('movement-allotment', [MovementAllotmentController::class, 'index']);
